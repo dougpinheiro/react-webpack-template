ea23356f1c251ddb75901273d6836eb2d9540c48
Douglas Pinheiro <dougcpinheiro@yahoo.com>
2018-11-05 11:15:31 +0000

    deleting front

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
+import {Card} from 'primereact/card';
 
 export default class App extends Component {
+
   render(){
+    const header = <img alt="Card" src='showcase/resources/demo/images/usercard.png'/>;
+    const footer = <span>
+                    <Button label="Save" icon="pi pi-check" style={{marginRight: '.25em'}}/>
+                    <Button label="Cancel" icon="pi pi-times" className="p-button-secondary"/>
+                </span>;
     return (
       <div>
-        Teste
+        <Card title="Advanced Card" header={header} footer={footer}>
+          <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur error repudiandae numquam deserunt
+                quisquam repellat libero asperiores earum nam nobis, culpa ratione quam perferendis esse, cupiditate neque quas!</div>
+        </Card>
       </div>
     )
   }
